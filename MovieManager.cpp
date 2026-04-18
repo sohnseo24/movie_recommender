0630acb4e3c86e73acf1ab3632e200ed686829eb
Seoyoung Sohn <sohnseo24@gmail.com>
2026-04-18 13:42:34 +0900

    M2: MovieManager 정렬(다중기준으로 수정) 및 제목으로 검색 기능 구현

--- MovieManager.cpp
+++ MovieManager.cpp
@@ -12,14 +12,30 @@ void MovieManager::printAllMovies() const {
         return;
     }
     for (const auto& m : movies) {
-        m.display(); // Movie 클래스의 display() 호출
+        std::cout << m << std::endl; 
     }
 }
 
+
 void MovieManager::sortByRating() {
     std::sort(movies.begin(), movies.end(), [](const Movie& a, const Movie& b) {
-        return a.getAverageRating() > b.getAverageRating(); 
+        // 평점이 다르면 평점 내림차순
+        if (a.getAverageRating() != b.getAverageRating()) {
+            return a.getAverageRating() > b.getAverageRating();
+        }
+        // 평점이 같으면 제목 가나다순(오름차순) 
+        return a.getTitle() < b.getTitle();
     });
     
-    std::cout << "평점순으로 영화를 정렬했습니다." << std::endl;
+    std::cout << "\n[알림] 평점순(다중 기준)으로 정렬되었습니다." << std::endl;
+}
+
+Movie* MovieManager::findByTitle(const std::string& title) {
+    for (Movie& m : movies) {
+        // Movie 클래스의 operator==사용
+        if (m == title) { 
+            return &m; // 찾으면 그 영화의 주소 전달
+        }
+    }
+    return nullptr; // 못 찾으면 nullptr 전달
 }
--- MovieManager.h
+++ MovieManager.h
@@ -7,7 +7,9 @@ private:
     std::vector<Movie> movies; // 영화 데이터를 담는 벡터
 
 public:
-    void addMovie(const Movie& m);
-    void printAllMovies() const;
-    void sortByRating(); // 평점순 정렬 함수
+    void addMovie(const Movie& m); //1번: 영화 추가
+    void printAllMovies() const;//3번: 전체 목록 출력
+    void sortByRating(); // 4번: 평점순 정렬 출력
+    Movie* findByTitle(const std::string& title); //2번: 제목으로 검색
 };
+
