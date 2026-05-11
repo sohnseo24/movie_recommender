b03ec4b5a814ebc37e1d3c4bad430d6882c43a09
Seoyoung Sohn <sohnseo24@gmail.com>
2026-05-11 21:04:24 +0900

    M3:RatingManager에 loadRatings추가 및 Rating관련 파일들 수정

--- include/RatingManager.h
+++ include/RatingManager.h
@@ -9,4 +9,6 @@ private:
 public:
     void addRating(const Rating& r); //7번: 평점 입력
     void printRatingsByMovie(int movieId) const; //8번: 영화별 평점 보기
+    void loadRatings(const std::string& filename);//프로그램 시작시 CSV파일에서 데이터를 로드한다.
+
 };
--- src/RatingManager.cpp
+++ src/RatingManager.cpp
@@ -1,5 +1,9 @@
+#include "Rating.h"
 #include "RatingManager.h"
 #include <iostream>
+#include <fstream>   // ifstream 사용을 위해
+#include <sstream>   // stringstream 사용을 위해
+#include <string>    // string, stoi, stod 사용을 위해
 
 void RatingManager::addRating(const Rating& r) {
     ratings.push_back(r);
@@ -15,4 +19,29 @@ void RatingManager::printRatingsByMovie(int movieId) const {
         }
     }
     if (count == 0) std::cout << "해당 영화에 대한 평점이 없습니다." << std::endl;
+}
+
+void RatingManager::loadRatings(const std::string& filename) { //CSV로딩을 위해 새로 추가
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Error: " << filename << " 열 수 없습니다" << std::endl;
+        return;
+    }
+
+    std::string line;
+    std::getline(file, line); // 헤더 스킵
+
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string token;
+
+        std::getline(ss, token, ','); int userId = std::stoi(token);
+        std::getline(ss, token, ','); int movieId = std::stoi(token);
+        std::getline(ss, token, ','); int score = std::stoi(token);
+
+        //읽어온 데이터를 바로 ratings 벡터에 추가!
+        ratings.push_back(Rating(userId, movieId, score)); 
+    }
+    std::cout << "[알림] " << filename << " 데이터를 성공적으로 불러왔습니다." << std::endl;
 }
