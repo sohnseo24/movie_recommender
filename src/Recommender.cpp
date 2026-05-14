961dd2924ccbe5cf48127bc9a0b6ed5e395f34b8
Seoyoung Sohn <sohnseo24@gmail.com>
2026-05-15 00:12:50 +0900

    M3:메인추천함수(recommend) 1,2,3,4단계 구현(STL set,map,sort,람다 사용)

--- src/Recommender.cpp
+++ src/Recommender.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
 #include "Recommender.h"
 #include <algorithm> // std::sort를 쓰기 위해 필요
-#include <iostream>
+#include <set>
+#include <map>
 using namespace std;
 
 vector<int> Recommender::findSimilarUsers(int targetUserId, int k,
@@ -10,7 +12,7 @@ const UserManager& um, const RatingManager& rm){
 
     //2) 내 평점 가져오기
     vector<Rating> myRatings = rm.findByUser(targetUserId);
-    //[엣지케이스 1번]: 평점이 0개인 사용자
+    //[엣지케이스 1번]: 평점이 0개인 사용자->유사한 사용자 찾을 수 없음
     if (myRatings.empty()){
         cout<< "평점 데이터가 부족하여 유사한 사용자를 찾을 수 없습니다"<<endl;
         return vector<int>(); //빈벡터 반환
@@ -43,3 +45,60 @@ const UserManager& um, const RatingManager& rm){
     }
     return topKUsers;
 }
+
+
+
+vector<Movie*> Recommender::recommend(int targetUserId, int k, int n,//n:최종 상위 몇개영화 반환할건지
+const MovieManager& mm, const UserManager& um, const RatingManager& rm){
+    //1단계(나): 내 평점 가져오기->내가 이미 본 영화 ID들 set에 저장(STL set활용->중복제거 및 빠른 검색)
+    vector<Rating> myRatings=rm.findByUser(targetUserId);
+    if (myRatings.empty()){//[엣지케이스 1번]: 평점이 0개인 사용자-> 나에 대한 정보가 없어 추천자체가 불가능
+        cout<<"평점 기록이 없어 추천을 진행할 수 없습니다."<<endl;
+        return {}; //빈 벡터 반환 
+    }
+    set<int> watchedMovieIds;
+    for(const auto& r: myRatings){
+        watchedMovieIds.insert(r.getMovieId());
+    }
+
+    //2~3단계(타인): 모든 사용자와의 유사도를 계산해서 상위 K명 선택, 후에 4단계에서 topKUsers에 저장된 Id를 friendId로 받아 findByUser로 그 friendId에 해당하는 타인의 평점을 friendRatings에 저장 
+    vector<int> topKUsers=findSimilarUsers(targetUserId, k, um, rm);
+    if(topKUsers.empty()) return{}; 
+
+    //4단계: 후보영화 수집 및 점수 누적(필터링:내가 안본 영화만 모으기=>가중평균:유사도가 높은 사용자들의 평점에 가중치주기
+    //=>정렬:점수높은 순으로 영화나열=>반환: 상위 N개만 골라 반환)
+    //STL map활용 map<영화 ID, 누적점수>
+    map<int, double> movieScores;
+
+    for(int friendId: topKUsers){
+        vector<Rating> friendRatings= rm.findByUser(friendId); //findByUser를 쓰면 그 사용자의 userRatings라는 벡터를 돌려줌
+        double similarity=SimilarityCalculator::calculate(myRatings,friendRatings);//가중치를 계산하기 위해서 친구와 나의 유사도를 다시 계산
+
+        for(const auto& r: friendRatings){
+            int movieId =r.getMovieId();//friendRatings는 RatingManager에 의해 userRatings벡터니까(즉 user의 모든 평점을 다 모아놓은 벡터니까 거기서 movieId를 뽑는다.)
+            
+            //필터링: 내가 안본 영화만 모아야 함(set 검색)
+            if(watchedMovieIds.find(movieId)== watchedMovieIds.end()){
+                //가중평균: 유사도 * 친구 평점을 점수로 누적
+                movieScores[movieId] +=(similarity * r.getScore());//map에 추가하면 이미 있는 아이디에 대해서는 누적해서 더하고, 없는 아이디에 대해서는 추가로 만들어서 저장한다.
+            }
+        }
+    }
+
+    //5단계: 점수계산&정렬반환 (sort,람다)
+    vector<pair<double, int>> finalRank;
+    for (auto const& [id, score]: movieScores){
+        finalRank.push_back({id, score});
+    }
+    sort(finalRank.begin(), finalRank.end(),[](const auto& a, const auto& b){
+        return a.second > b.second; //점수가 큰 순서대로 정렬(내림차순)
+    });
+    //상위 n개의 Movie객체 포인터 담기
+    vector<Movie*> result;
+    for(int i=0; i<n&&i<finalRank.size(); i++){
+        
+    }
+}
+//<Movie*>는 Movie객체가 메모리 어디에 있는지 가리키는 주소값(포인터)를 의미
+//Movie객체 자체(용량이 큼)를 복사하면 비효율적이므로 주소만 넘겨주는 것
+//MovieManager가 이미 모든 영화 정보를 들고 있기 때문에 recommend는 그냥 이 영화가 좋다 추천하는 역할-> 주소만 콕 집어서 알려주는게 훨씬 효율적
