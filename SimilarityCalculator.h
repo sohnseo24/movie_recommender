ef6f68daa8e1ebb0e01406df90823d611fc72e76
Seoyoung Sohn <sohnseo24@gmail.com>
2026-05-18 22:21:43 +0900

    M3: 쓸모없는 파일 정리2

--- SimilarityCalculator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <Rating.h>
-#include <vector>
-
-class SimularityCarculator{
-    int calculate(const std::vector<Rating>& user1, const std::vector<Rating>& user2);
-};
